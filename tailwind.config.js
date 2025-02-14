b9b65ba4921743769bbf2e133d61dbd64093c583
Joshua Shiman <joshshiman@proton.me>
2025-02-14 12:21:37 -0500

    Reset repository

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
 	prefix: 'tw-',
 	important: false,
 	content: [
-		"./**/*.{html, jsx, js}",
+		"**/*.{html, jsx, js}",
+		"**/*.js",
+		"**/*.html",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "#000"
+			}
+		},
 	},
 	plugins: [],
 }
+
